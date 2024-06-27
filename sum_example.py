0d5439a9995e346e211bab5acaa7732e38a1aeb1
Admin <admin@Admins-MacBook-Air.local>
2024-06-27 11:08:19 -0700

    swing file

---
+++ sum_example.py
@@ -0,0 +1,8 @@
+#The function sums from one important number all the way to the other important number
+def sum(num):
+    for i in range(num):
+            num = num + i 
+    print(num)
+
+sum(5)
+sum(10)
